60074bf8fe1ea49f97a740ec7cc214616992b824
squidcarroll <squidcarroll@gmail.com>
2018-05-18 16:41:05 -0700

    push status

--- static/jquery.js
+++ static/jquery.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $("#stopbtn").click(function() {
         if ($(this).hasClass("ui red green button")) {
-            $.post("/dashboard", { status: "0" });
+            $.post("/dashboard", { status: "0" }, "json");
         } else {
-            $.post("/dashboard", { status: "1" });
+            $.post("/dashboard", { status: "1" }, "json");
         }
         $('#stopmsg').text(function(i, text){
             return text == "Would you like to give the bot a rest?" ? "How about you start up the bot!" : "Would you like to give the bot a rest?";
