164a928e43464ba0ba8895dec9df57e4297d0449
Jesse Hedden <jagarvargen@Jesses-MacBook-Pro.local>
2018-05-18 14:30:07 -0700

    adding entry point

--- models/bots.py
+++ models/bots.py
@@ -1,71 +1,28 @@
-#!/usr/bin/python
 '''
-   Create a timed query of database
+    Sets the timer for querying the database and making retweets
 '''
 from sched import scheduler
-import time, os, flask
-from sqlite3 import dbapi2 as sqlite
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from models.user import User, Base
+import time
+from models.storage import DBStorage
 
 
-''' Instantiates the scheduler '''
-timer = scheduler(time.time, time.sleep)
+# Open Database
+db = DBStorage()
+db.reload()
 
-'''Set the timed query for every hour from instantiation and start timer '''
-timer.enter(3600, 1, ''' NEED TO ENTER FUNCTION ''')
-timer.run()
+# Set scheduler to query user data and send tweets
+schedule = scheduler(time.time, time.sleep)
+schedule.enter(3600, 1, main_entry)
+schedule.run()
+    
+def main_entry():
+    ''' Initiates everything based on timer '''
+    users = db.query_active_users()
 
-class DBStorage:
-    ''' Read from and write to DB '''
-    __engine = None
-    __session = None
+    for user in users:
+        keywords = user.keywords.replace('|', ' ').split()
+        search_tweets(user.access_token_key, user.access_token_secret, keywords)
 
-    def __init__(self):
-        '''
-            DBStorage Contructor 
-        '''
-        self.__engine = create_engine(
-        ‘sqlite+pysqlite:///jacksbot.db’,
-        native_datetime=True,
-        module=sqlite,
-        pool_pre_ping=True, echo=False)
+    db.close()
 
-def query_users(self, user_ids=[]):
-    '''
-        Retrieve all user objects from db
-    '''
-
-
-def add_user(self, obj):
-    '''
-        Add new User to database
-    '''
-    self.__session.add(obj)
-    self.__session.commit()
-
-def delete(self, obj=None):
-    '''
-        Delete User from db
-    '''
-    if obj:
-        self.__session.delete(obj)
-
-def reload(self):
-    '''
-        Create the db session
-    '''
-    Base.metadata.create_all(self.__engine)
-    session_maker = sessionmaker(
-        bind=self.__engine,
-        expire_on_commit=False
-    )
-    self.__session = scoped_session(session_maker)
-
-def close(self):
-    '''
-        Close the db session
-    '''
-    self.__session.close()
+    return schedule.queue
